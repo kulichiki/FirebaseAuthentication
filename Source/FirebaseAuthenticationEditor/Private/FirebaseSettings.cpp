113fe104cfb9b9665ccd4733c9bda69af9c40fcc
kulichin <thedark174@gmail.com>
2020-08-14 00:13:32 +0300

    Removed unused code

--- Source/FirebaseAuthenticationEditor/Private/FirebaseSettings.cpp
+++
@@ -1,9 +0,0 @@
-#include "FirebaseSettings.h"
-
-#if WITH_EDITOR
-void UFirebaseSettings::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
-{
-	Super::PostEditChangeProperty(PropertyChangedEvent);
-	SaveConfig(CPF_Config, *GetDefaultConfigFilename());
-}
-#endif
